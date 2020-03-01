beb128432fb612693a634bd4d324ac07752668f2
Yuzunoha <studysteadystandby@gmail.com>
2020-03-01 23:47:46 +0900

    テストページ追加。laravelの変更が持ち運べるか

--- docker-laravel/src/routes/web.php
+++ docker-laravel/src/routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test', function () {
+    $s = "";
+    $s .= "ここは<br>";
+    $s .= "テストページ<br>";
+    $s .= "です<br>";
+    return $s;
+});
